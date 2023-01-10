3a4bbce9ec13b6bc5291640c234e7c6c0376fec3
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2023-01-11 00:44:30 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -14,30 +14,29 @@ namespace TypewiseAlert.Test
             //If Mail/message to Controller sent = 1; If Mail not Sent since NORMAL = 2 ; If Failure = 0
             Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 0) == 2); //normal - no email
             Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 41) == 2);//normal - no email
-            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 100) == 1);//high value - email
+            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 101) == 1);//high value - email
             Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, -4) == 1);//low value - email
 
-            Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 105) == 1);
+            Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 106) == 1);
             Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, -5) == 1);
 
             batteryCharacter.coolingType = CoolingType.MED_ACTIVE_COOLING;
 
-            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 0) == 2); //normal - no email
-            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 41) == 1);//high - email
-            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, -4) == 1);//low value - email
+            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 42) == 1);//high - email
+            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, -7) == 1);//low value - email
 
             Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 105) ==1);
-            Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, -5) == 1);
+            Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, -3) == 1);
 
 
             batteryCharacter.coolingType = CoolingType.PASSIVE_COOLING;
 
             Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 0) == 2); //normal - no email
             Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 100) == 1);//high value - email
-            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, -4) == 1);//low value - email
+            Assert.True(checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, -9) == 1);//low value - email
 
             Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 105) == 1);
-            Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, -5) == 1);
+            Assert.True(checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, -6) == 1);
 
         }
 
